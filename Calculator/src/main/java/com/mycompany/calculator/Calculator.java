170963b15214ce12ccfc09ec23092685d96dd5da
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2024-03-06 09:29:41 -0600

    Hice la función resta
    para restar dos números.

--- Calculator/src/main/java/com/mycompany/calculator/Calculator.java
+++ Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
 public static int sum(int numberUno,int numberTwo){
     return numberUno + numberTwo;
 }
+public static int rest(int numberUno,int numberTwo){
+    return numberUno - numberTwo;
+}
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
