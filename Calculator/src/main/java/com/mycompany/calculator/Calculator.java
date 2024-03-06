194445221979b34f4327459533d282449a3875ac
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2024-03-06 09:22:24 -0600

    Hice la función sumar
    para sumar dos números.

--- Calculator/src/main/java/com/mycompany/calculator/Calculator.java
+++ Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package com.mycompany.calculator;
 
 public class Calculator {
+public static int sum(int numberUno,int numberTwo){
+    return numberUno + numberTwo;
+}
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
