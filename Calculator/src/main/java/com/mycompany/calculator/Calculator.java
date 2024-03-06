30ddcdbfb68ab1a389da11bfa166d548c4e913b6
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2024-03-06 09:55:37 -0600

    Hice la función dividir
    para dividir dos números y también verificar el error que no se pueda dividir por cero

--- Calculator/src/main/java/com/mycompany/calculator/Calculator.java
+++ Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -10,7 +10,12 @@ public static int rest(int numberUno,int numberTwo){
 public static int multiplication(int numberUno,int numberTwo){
     return numberUno * numberTwo;
 }
-
+public static float division(int numberUno,int numberTwo){
+    if (numberTwo == 0) {
+         throw new ArithmeticException(" No se puede dividir por cero ");
+    }
+    return (float)numberUno / numberTwo;
+}
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
